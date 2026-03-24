eb90370ea6736a764056857b21388a87587ecf26
Bilal Aamir <bilalaamirr@gmail.com>
2026-03-24 18:21:43 +0500

    Updated training and visualization &  Generated Confusion Matrix

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pickle
 from preprocess import load_and_preprocess
 from model import KNN
 from sklearn.metrics import accuracy_score
+from visualize import plot_confusion_matrix
 
 # Load data
 (X_train, X_test, y_train, y_test), scaler = load_and_preprocess("../data/student_scores.csv")
@@ -16,6 +17,8 @@ y_pred = model.predict(X_test)
 # Accuracy
 acc = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {acc}")
+
+plot_confusion_matrix(y_test, y_pred)
 
 # Save model
 with open("../model.pkl", "wb") as f:
--- src/visualize.py
+++ src/visualize.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
-import numpy as np
-
+import os
 
 def plot_confusion_matrix(y_test, y_pred):
+    os.makedirs("../results", exist_ok=True)  # ✅
+
     cm = confusion_matrix(y_test, y_pred, labels=["Good", "Average", "Poor"])
 
-    sns.heatmap(cm, annot=True, fmt="d", xticklabels=["Good", "Avg", "Poor"],
+    sns.heatmap(cm, annot=True, fmt="d",
+                xticklabels=["Good", "Avg", "Poor"],
                 yticklabels=["Good", "Avg", "Poor"])
 
     plt.title("Confusion Matrix")
+    plt.tight_layout()  # ✅
     plt.savefig("../results/confusion_matrix.png")
     plt.clf()
